f13a01fcd803721d4e069e450873070c7e63b35c
Carlos Lloret <clloretplayan@gmail.com>
2020-06-02 21:43:41 +0200

    Properly formatting the exercise attempt date time

--- app/src/main/java/com/clloret/speakingpractice/attempt/list/AttemptListViewHolder.kt
+++ app/src/main/java/com/clloret/speakingpractice/attempt/list/AttemptListViewHolder.kt
@@ -3,13 +3,17 @@ package com.clloret.speakingpractice.attempt.list
 import androidx.recyclerview.widget.RecyclerView
 import com.clloret.speakingpractice.databinding.AttemptListItemBinding
 import com.clloret.speakingpractice.domain.entities.ExerciseAttempt
+import java.text.DateFormat
 
 class AttemptListViewHolder(private val binding: AttemptListItemBinding) :
     RecyclerView.ViewHolder(binding.root) {
     fun bind(item: ExerciseAttempt) {
+        val df = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.SHORT)
+        val formattedTime = df.format(item.time)
+
         binding.apply {
             attempt = item
-            attemptTime = item.time.toString()
+            attemptTime = formattedTime
         }
     }
 }

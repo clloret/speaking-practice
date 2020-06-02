aff7b5f5aa6c17655233e2cad830d7d333ec64cf
Carlos Lloret <clloretplayan@gmail.com>
2020-06-02 19:31:00 +0200

    Add divider between exercise list items

--- app/src/main/java/com/clloret/speakingpractice/exercise/list/ExerciseListFragment.kt
+++ app/src/main/java/com/clloret/speakingpractice/exercise/list/ExerciseListFragment.kt
@@ -7,9 +7,12 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.clloret.speakingpractice.R
 import com.clloret.speakingpractice.databinding.ExerciseListFragmentBinding
+
 
 class ExerciseListFragment : Fragment() {
 
@@ -34,18 +37,29 @@ class ExerciseListFragment : Fragment() {
         )
 
         binding.lifecycleOwner = viewLifecycleOwner
-        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerView.setupRecyclerView()
 
-        val adapter = ExerciseListAdapter(findNavController())
-        binding.recyclerView.adapter = adapter
+        return binding.root
+    }
+
+    private fun RecyclerView.setupRecyclerView() {
+        val linearLayoutManager = LinearLayoutManager(requireContext())
+        layoutManager = linearLayoutManager
+
+        val dividerItemDecoration = DividerItemDecoration(
+            context,
+            linearLayoutManager.orientation
+        )
+        addItemDecoration(dividerItemDecoration)
+
+        val listAdapter = ExerciseListAdapter(findNavController())
+        adapter = listAdapter
 
         viewModel.exercises.observe(viewLifecycleOwner, Observer {
             it?.let {
-                adapter.submitList(it)
+                listAdapter.submitList(it)
             }
         })
-
-        return binding.root
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {

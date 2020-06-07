8752edd9c310c33d28faf57afcd1ac092a21ebe1
Carlos Lloret <clloretplayan@gmail.com>
2020-06-07 12:52:09 +0200

    Add practice speaking button to home screen

--- app/src/main/java/com/clloret/speakingpractice/home/HomeFragment.kt
+++ app/src/main/java/com/clloret/speakingpractice/home/HomeFragment.kt
@@ -5,11 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.clloret.speakingpractice.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
-/**
- * A simple [Fragment] subclass.
- */
 class HomeFragment : Fragment() {
 
     override fun onCreateView(
@@ -18,6 +17,19 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btnPractice.setOnClickListener {
+            val action =
+                HomeFragmentDirections.actionHomeFragmentToExerciseFragment()
+
+            findNavController()
+                .navigate(action)
+        }
+
     }
 
 }

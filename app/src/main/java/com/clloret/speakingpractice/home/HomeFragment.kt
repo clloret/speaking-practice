e29b2bd04aa4f3f5f7629c5bb2273517b1055239
Carlos Lloret <clloretplayan@gmail.com>
2020-06-07 11:03:00 +0200

    Add HomeFragment

---
+++ app/src/main/java/com/clloret/speakingpractice/home/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.clloret.speakingpractice.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.clloret.speakingpractice.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+}

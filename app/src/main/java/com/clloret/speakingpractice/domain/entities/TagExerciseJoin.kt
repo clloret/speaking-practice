922937bfcc5737c14f32070b70362de1f729ef0e
Carlos Lloret <clloretplayan@gmail.com>
2020-06-11 17:51:51 +0200

    Add TagExerciseJoin index

--- app/src/main/java/com/clloret/speakingpractice/domain/entities/TagExerciseJoin.kt
+++ app/src/main/java/com/clloret/speakingpractice/domain/entities/TagExerciseJoin.kt
@@ -1,22 +1,25 @@
 package com.clloret.speakingpractice.domain.entities
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
 
 @Entity(
     tableName = "tag_exercise_join",
-    primaryKeys = ["tagId", "exerciseId"],
+    primaryKeys = ["tag_id", "exercise_id"],
     foreignKeys = [ForeignKey(
         entity = Tag::class,
         parentColumns = arrayOf("id"),
-        childColumns = arrayOf("tagId")
+        childColumns = arrayOf("tag_id")
     ), ForeignKey(
         entity = Exercise::class,
         parentColumns = arrayOf("id"),
-        childColumns = arrayOf("exerciseId")
+        childColumns = arrayOf("exercise_id")
     )]
 )
 data class TagExerciseJoin(
+    @ColumnInfo(name = "tag_id", index = true)
     val tagId: Int,
+    @ColumnInfo(name = "exercise_id", index = true)
     val exerciseId: Int
 )

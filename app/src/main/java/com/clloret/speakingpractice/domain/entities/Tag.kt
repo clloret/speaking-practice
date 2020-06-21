94f5bcfe9808103fb3fafe50e998f9f9c49f6c09
Carlos Lloret <clloretplayan@gmail.com>
2020-06-21 12:56:00 +0200

    Add name as unique key to the tag entity

--- app/src/main/java/com/clloret/speakingpractice/domain/entities/Tag.kt
+++ app/src/main/java/com/clloret/speakingpractice/domain/entities/Tag.kt
@@ -2,10 +2,13 @@ package com.clloret.speakingpractice.domain.entities
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import com.clloret.speakingpractice.utils.databinding.ChipEntryBinding
 
-@Entity(tableName = "tags")
+@Entity(
+    tableName = "tags", indices = [Index(value = ["name"], unique = true)]
+)
 data class Tag(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "tag_id")

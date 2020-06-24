89cb99a0669602c7125e8f92d1a57f9338973a80
Carlos Lloret <clloretplayan@gmail.com>
2020-06-24 21:50:44 +0200

    Add success rate to ExerciseResults

--- app/src/main/java/com/clloret/speakingpractice/domain/entities/ExerciseResults.kt
+++ app/src/main/java/com/clloret/speakingpractice/domain/entities/ExerciseResults.kt
@@ -2,6 +2,7 @@ package com.clloret.speakingpractice.domain.entities
 
 import androidx.room.ColumnInfo
 import androidx.room.DatabaseView
+import kotlin.math.roundToInt
 
 @DatabaseView(
     viewName = "exercise_results",
@@ -9,7 +10,7 @@ import androidx.room.DatabaseView
         SELECT 
             exercises.exercise_id,
             SUM(result) AS correct, 
-            COUNT(*) - SUM(result) AS incorrect 
+            COUNT(*) - SUM(result) AS incorrect
         FROM 
             exercises
             LEFT OUTER JOIN exercise_attempts ON exercises.exercise_id = exercise_attempts.exercise_id 
@@ -19,4 +20,11 @@ data class ExerciseResults(
     @ColumnInfo(name = "exercise_id") val id: Int,
     val correct: Int,
     val incorrect: Int
-)
+) {
+    val successRate: Int
+        get() {
+            val count = (correct + incorrect).takeIf { it > 0 } ?: return 0
+
+            return (correct * 100 / count.toDouble()).roundToInt()
+        }
+}

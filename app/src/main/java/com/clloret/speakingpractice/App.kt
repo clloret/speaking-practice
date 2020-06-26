0933f0a2922eea14b50096e0bc89d6981b3aac84
Carlos Lloret <clloretplayan@gmail.com>
2020-06-26 20:29:52 +0200

    Configure Koin

--- app/src/main/java/com/clloret/speakingpractice/App.kt
+++ app/src/main/java/com/clloret/speakingpractice/App.kt
@@ -1,11 +1,52 @@
 package com.clloret.speakingpractice
 
 import android.app.Application
+import com.clloret.speakingpractice.db.ExerciseRepository
+import com.clloret.speakingpractice.db.ExercisesDatabase
+import kotlinx.coroutines.CompletableJob
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidFileProperties
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
 import timber.log.Timber
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        setupLog()
+        setupKoin()
+    }
+
+    private fun setupKoin() {
+        val appModule = module {
+
+            single { ExercisesDatabase.getDatabase(get(), get()) }
+            single { ExerciseRepository(get()) }
+
+            factory { SupervisorJob() }
+            factory { CoroutineScope(get<CompletableJob>()) }
+        }
+
+        startKoin {
+            // use AndroidLogger as Koin Logger - default Level.INFO
+            androidLogger()
+
+            // use the Android context given there
+            androidContext(this@App)
+
+            // load properties from assets/koin.properties file
+            androidFileProperties()
+
+            // module list
+            modules(appModule)
+        }
+    }
+
+    private fun setupLog() {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }

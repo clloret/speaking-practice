9d9a85690a9bb8116c3e63e56df827d445a97456
Carlos Lloret <clloretplayan@gmail.com>
2020-06-27 09:23:21 +0200

    Remove unused code

--- app/src/main/java/com/clloret/speakingpractice/App.kt
+++ app/src/main/java/com/clloret/speakingpractice/App.kt
@@ -14,7 +14,6 @@ import kotlinx.coroutines.CompletableJob
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidFileProperties
 import org.koin.android.ext.koin.androidLogger
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
@@ -53,9 +52,6 @@ class App : Application() {
 
             // use the Android context given there
             androidContext(this@App)
-
-            // load properties from assets/koin.properties file
-            androidFileProperties()
 
             // module list
             modules(appModule)

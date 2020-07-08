84821ff18155035de8697722449d18f3743bc890
Carlos Lloret <clloretplayan@gmail.com>
2020-07-08 19:42:49 +0200

    Set success rate text color depending of the percent value

--- app/src/main/java/com/clloret/speakingpractice/exercise/practice/PracticeViewHolder.kt
+++ app/src/main/java/com/clloret/speakingpractice/exercise/practice/PracticeViewHolder.kt
@@ -1,6 +1,7 @@
 package com.clloret.speakingpractice.exercise.practice
 
 import androidx.recyclerview.widget.RecyclerView
+import com.clloret.speakingpractice.R
 import com.clloret.speakingpractice.databinding.PracticeItemBinding
 import com.clloret.speakingpractice.domain.entities.ExerciseWithDetails
 
@@ -12,7 +13,14 @@ class PracticeViewHolder(private val binding: PracticeItemBinding) :
     ) {
         binding.apply {
             exercise = item
+            successRateColor = getSuccessRateColor(item.results.successRate)
             model = viewModel
         }
+    }
+
+    private fun getSuccessRateColor(successRate: Int) = when (successRate) {
+        in 0..49 -> R.color.success_rate_failed
+        in 50..79 -> R.color.success_rate_passed
+        else -> R.color.success_rate_completed
     }
 }

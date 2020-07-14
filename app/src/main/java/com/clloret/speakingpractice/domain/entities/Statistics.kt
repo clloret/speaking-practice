ce0e28946e259eeb7734cb5edf12ed0974e1879b
Carlos Lloret <clloretplayan@gmail.com>
2020-07-14 20:42:04 +0200

    Add statistics entity

---
+++ app/src/main/java/com/clloret/speakingpractice/domain/entities/Statistics.kt
@@ -0,0 +1,16 @@
+package com.clloret.speakingpractice.domain.entities
+
+import androidx.room.ColumnInfo
+
+data class Statistics(
+    @ColumnInfo(name = "total_attempts") val totalAttempts: Int,
+    @ColumnInfo(name = "correct_attempts") val correctAttempts: Int,
+    @ColumnInfo(name = "incorrect_attempts") val incorrectAttempts: Int,
+    @ColumnInfo(name = "success_rate") val successRate: Int,
+    @ColumnInfo(name = "total_exercises") val totalExercises: Int,
+    @ColumnInfo(name = "practiced_exercises") val practicedExercises: Int,
+    @ColumnInfo(name = "non_practiced_exercises") val nonPracticedExercises: Int
+) {
+    val percentagePracticedExercises: Int
+        get() = practicedExercises / totalExercises
+}

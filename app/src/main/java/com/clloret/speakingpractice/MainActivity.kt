dc14f05d217fa3d4e72778250f0cc69e85a1ea6e
Carlos Lloret <clloretplayan@gmail.com>
2020-08-08 17:50:20 +0200

    Remove main menu

--- app/src/main/java/com/clloret/speakingpractice/MainActivity.kt
+++ app/src/main/java/com/clloret/speakingpractice/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.clloret.speakingpractice
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
@@ -39,22 +37,6 @@ class MainActivity : AppCompatActivity() {
             EventObserver {
                 showSnackBar(it)
             })
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 
     private fun showSnackBar(message: String) {

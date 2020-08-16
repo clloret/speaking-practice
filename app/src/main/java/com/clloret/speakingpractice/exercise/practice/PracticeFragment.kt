81e99d0f8ac495cea5c7c95cd050936bd8c438d7
Carlos Lloret <clloretplayan@gmail.com>
2020-08-16 21:24:58 +0200

    Set the recognize speech language to en-US

--- app/src/main/java/com/clloret/speakingpractice/exercise/practice/PracticeFragment.kt
+++ app/src/main/java/com/clloret/speakingpractice/exercise/practice/PracticeFragment.kt
@@ -167,7 +167,7 @@ class PracticeFragment : Fragment() {
                 RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                 RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
             )
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.US.toLanguageTag())
         intent.putExtra(
             RecognizerIntent.EXTRA_PROMPT,
             getString(R.string.msg_read_exercise_sentence)

c7247e489aafebb5088038678d3f0d3e19d232e4
Carlos Lloret <clloretplayan@gmail.com>
2020-08-27 20:35:02 +0200

    Add popup menu to word list screen

--- app/src/main/java/com/clloret/speakingpractice/word/WordListAdapter.kt
+++ app/src/main/java/com/clloret/speakingpractice/word/WordListAdapter.kt
@@ -1,13 +1,17 @@
 package com.clloret.speakingpractice.word
 
+import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.PopupMenu
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.SortedList
 import com.clloret.speakingpractice.R
 import com.clloret.speakingpractice.databinding.WordListItemBinding
+import com.clloret.speakingpractice.domain.attempt.filter.AttemptFilterByWord
 import com.clloret.speakingpractice.domain.entities.PracticeWordWithResults
 import com.clloret.speakingpractice.domain.exercise.filter.ExerciseFilterByWord
 import com.clloret.speakingpractice.domain.word.sort.WordSortable
@@ -68,6 +72,29 @@ class WordListAdapter(
         findNavController.navigate(action)
     }
 
+    override fun onClickMenu(view: View, word: PracticeWordWithResults) {
+        PopupMenu(view.context, view).apply {
+            setOnMenuItemClickListener {
+                when (it.itemId) {
+                    R.id.action_show_attempts -> showExerciseAttempts(word)
+                    else -> false
+                }
+            }
+            inflate(R.menu.menu_word_list_popup)
+            gravity = Gravity.START
+            show()
+        }
+    }
+
+    private fun showExerciseAttempts(word: PracticeWordWithResults): Boolean {
+        val action = WordListFragmentDirections.actionWordListFragmentToAttemptListFragment(
+            AttemptFilterByWord(word.word)
+        )
+        findNavController.navigate(action)
+
+        return true
+    }
+
     class ViewHolder(private val binding: WordListItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
@@ -86,4 +113,5 @@ class WordListAdapter(
 
 interface OnClickWordHandler {
     fun onClick(word: PracticeWordWithResults)
+    fun onClickMenu(view: View, word: PracticeWordWithResults)
 }

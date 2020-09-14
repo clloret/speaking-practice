9f9368fd635e796014a96323878685fff0584fa1
Carlos Lloret <clloretplayan@gmail.com>
2020-09-14 20:24:03 +0200

    Add CircularProgressBar control to show the attempts practiced each day

---
+++ app/src/main/java/com/clloret/speakingpractice/utils/controls/CircularProgressBar.kt
@@ -0,0 +1,70 @@
+package com.clloret.speakingpractice.utils.controls
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import com.clloret.speakingpractice.R
+import kotlinx.android.synthetic.main.circular_progress_bar_view.view.*
+
+class CircularProgressBar : LinearLayout {
+
+    var progress: Int = 0
+        set(value) {
+            progressBar?.progress = value
+            field = value
+        }
+
+    var max: Int = 0
+        set(value) {
+            progressBar?.max = value
+            field = value
+        }
+
+    var value: String? = null
+        set(value) {
+            textView?.text = value
+            field = value
+        }
+
+    constructor(context: Context) : super(context) {
+        initializeViews(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        setStyledAttributes(context, attrs)
+        initializeViews(context)
+    }
+
+    constructor(
+        context: Context,
+        attrs: AttributeSet?,
+        defStyle: Int
+    ) : super(context, attrs, defStyle) {
+        setStyledAttributes(context, attrs)
+        initializeViews(context)
+    }
+
+    private fun setStyledAttributes(context: Context, attrs: AttributeSet?) {
+        context.obtainStyledAttributes(attrs, R.styleable.CircularProgressBar).apply {
+            max = getInt(R.styleable.CircularProgressBar_max, max)
+            progress = getInt(R.styleable.CircularProgressBar_progress, progress)
+            value = getString(R.styleable.CircularProgressBar_value)
+            recycle()
+        }
+    }
+
+    private fun initializeViews(context: Context) {
+        val inflater = context
+            .getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        inflater.inflate(R.layout.circular_progress_bar_view, this)
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+
+        progressBar.max = max
+        progressBar.progress = progress
+        textView.text = value
+    }
+}

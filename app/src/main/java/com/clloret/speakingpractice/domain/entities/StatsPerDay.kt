6b93db197d7d00e707cdf5934ac3f327a4823b47
Carlos Lloret <clloretplayan@gmail.com>
2020-09-19 14:21:31 +0200

    Remove unused code

--- app/src/main/java/com/clloret/speakingpractice/domain/entities/StatsPerDay.kt
+++ app/src/main/java/com/clloret/speakingpractice/domain/entities/StatsPerDay.kt
@@ -7,8 +7,4 @@ data class StatsPerDay(
     @ColumnInfo(name = "total_attempts") val totalAttempts: Int,
     @ColumnInfo(name = "correct_attempts") val correctAttempts: Int,
     @ColumnInfo(name = "incorrect_attempts") val incorrectAttempts: Int
-    //@ColumnInfo(name = "success_rate") val successRate: Int
 )
-//    val percentagePracticedExercises: Int
-//        get() = (practicedExercises * 100 / totalExercises.toDouble()).roundToInt()
-//}
